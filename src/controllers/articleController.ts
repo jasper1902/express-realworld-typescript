3948b3608f6c76e04d4c7dabfa60a850ffe94a03
Jasper2002 <earth1887@gmail.com>
2023-02-27 05:32:05 +0700

    fix article response

--- src/controllers/articleController.ts
+++ src/controllers/articleController.ts
@@ -3,165 +3,190 @@ import Article from "../models/Article";
 import { RequestHandler } from "express";
 import { JWTNewRequest } from "./profileController";
 import slugify from "slugify";
-import createHttpError from "http-errors";
 
-export const createArticle: RequestHandler = async (req, res) => {
-  const newReq = req as unknown as JWTNewRequest;
-  const id = newReq.userId;
-  console.log(id);
+export const createArticle: RequestHandler = async (req, res, next) => {
+  try {
+    const newReq = req as unknown as JWTNewRequest;
+    const id = newReq.userId;
+    console.log(id);
 
-  const author = await User.findById(id);
+    const author = await User.findById(id);
 
-  if (!author) {
-    return res.status(401).json({ message: "User not found" });
+    if (!author) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    const { title, description, body, tagList } = req.body.article;
+
+    if (!title || !description || !body) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const slug = slugify(title, { lower: true, replacement: "-" });
+
+    const existingSlug = await Article.findOne({ slug }).exec();
+    if (existingSlug) {
+      return res.status(400).json({ message: "Article already exists" });
+    }
+
+    const article = await Article.create({ title, description, body, slug });
+    article.author = author._id;
+
+    if (Array.isArray(tagList) && tagList.length > 0) {
+      article.tagList = tagList;
+    }
+
+    await article.save();
+
+    await res.status(201).json({
+      article: await article.toArticleResponse(author),
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const { title, description, body, tagList } = req.body.article;
-
-  if (!title || !description || !body) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-  const slug = slugify(title, { lower: true, replacement: "-" });
-
-  const existingSlug = await Article.findOne({ slug }).exec();
-  if (existingSlug) {
-    return res.status(400).json({ message: "Article already exists" });
-  }
-
-  const article = await Article.create({ title, description, body, slug });
-  article.author = author._id;
-
-  if (Array.isArray(tagList) && tagList.length > 0) {
-    article.tagList = tagList;
-  }
-
-  await article.save();
-
-  return await res.status(201).json({
-    article: await article.toArticleResponse(author),
-  });
 };
 
-export const deleteArticle: RequestHandler = async (req, res) => {
-  const newReq = req as unknown as JWTNewRequest;
-  const id = newReq.userId;
+export const deleteArticle: RequestHandler = async (req, res, next) => {
+  try {
+    const newReq = req as unknown as JWTNewRequest;
+    const id = newReq.userId;
 
-  const slug = req.params.slug;
+    const slug = req.params.slug;
 
-  const loginUser = await User.findById(id);
+    const loginUser = await User.findById(id);
 
-  if (!loginUser) {
-    return res.status(404).json({ message: "User not found" });
+    if (!loginUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const article = await Article.findOne({ slug }).exec();
+
+    if (!article) {
+      return res.status(404).json({ message: "Article not found" });
+    }
+
+    if (article.author.toString() === loginUser._id.toString()) {
+      await Article.deleteOne({ slug });
+      res.status(200).json({
+        message: "Article successfully deleted!!!",
+      });
+    } else {
+      res.status(403).json({
+        message: "Only the author can delete his article",
+      });
+    }
+  } catch (error) {
+    next(error);
   }
+};
 
-  const article = await Article.findOne({ slug }).exec();
+export const favoriteArticle: RequestHandler = async (req, res, next) => {
+  try {
+    const newReq = req as unknown as JWTNewRequest;
+    const id = newReq.userId;
+    const { slug } = req.params;
 
-  if (!article) {
-    return res.status(404).json({ message: "Article not found" });
+    const loginUser = await User.findById(id).exec();
+
+    if (!loginUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const article = await Article.findOne({ slug }).exec();
+
+    if (!article) {
+      return res.status(404).json({ message: "Article not found" });
+    }
+
+    await loginUser.favorite(article._id);
+    const updatedArticle = await article.updateFavoriteCount();
+
+    return res.status(200).json({
+      article: await updatedArticle.toArticleResponse(loginUser),
+    });
+  } catch (error) {
+    next(error);
   }
+};
 
-  if (article.author.toString() === loginUser._id.toString()) {
-    await Article.deleteOne({ slug });
+export const unfavoriteArticle: RequestHandler = async (req, res, next) => {
+  try {
+    const newReq = req as unknown as JWTNewRequest;
+    const id = newReq.userId;
+    const { slug } = req.params;
+
+    const loginUser = await User.findById(id).exec();
+
+    if (!loginUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const article = await Article.findOne({ slug }).exec();
+
+    if (!article) {
+      return res.status(404).json({ message: "Article not found" });
+    }
+
+    await loginUser.unfavorite(article._id);
+    const updatedArticle = await article.updateFavoriteCount();
+
+    return res.status(200).json({
+      article: await updatedArticle.toArticleResponse(loginUser),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getArticleWithSlug: RequestHandler = async (req, res, next) => {
+  try {
+    const { slug } = req.params;
+    const article = await Article.findOne({ slug }).exec();
+
+    if (!article) {
+      return res.status(404).json({ message: "Article not found" });
+    }
+
     res.status(200).json({
-      message: "Article successfully deleted!!!",
+      article: await article.toArticleResponse(false),
     });
-  } else {
-    res.status(403).json({
-      message: "Only the author can delete his article",
-    });
+  } catch (error) {
+    next(error);
   }
 };
 
-export const favoriteArticle: RequestHandler = async (req, res) => {
-  const newReq = req as unknown as JWTNewRequest;
-  const id = newReq.userId;
-  const { slug } = req.params;
+export const updateArticle: RequestHandler = async (req, res, next) => {
+  try {
+    const newReq = req as unknown as JWTNewRequest;
+    const id = newReq.userId;
+    const { slug } = req.params;
+    const { article } = req.body;
 
-  const loginUser = await User.findById(id).exec();
+    const loginUser = await User.findById(id).exec();
 
-  if (!loginUser) {
-    throw createHttpError(404, "User not found");
-  }
+    if (!loginUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const target = await Article.findOne({ slug }).exec();
 
-  const article = await Article.findOne({ slug }).exec();
+    if (loginUser._id.toString() !== target?.author.toString()) {
+      return res
+        .status(401)
+        .json({ message: "You are not allowed to update this article" });
+    }
 
-  if (!article) {
-    throw createHttpError(404, "Article not found");
-  }
+    if (target && article) {
+      if (article.title) target.title = article.title;
+      if (article.description) target.description = article.description;
+      if (article.body) target.body = article.body;
 
-  await loginUser.favorite(article._id);
-  const updatedArticle = await article.updateFavoriteCount();
-
-  return res.status(200).json({
-    article: await updatedArticle.toArticleResponse(loginUser),
-  });
-};
-
-export const unfavoriteArticle: RequestHandler = async (req, res) => {
-  const newReq = req as unknown as JWTNewRequest;
-  const id = newReq.userId;
-  const { slug } = req.params;
-
-  const loginUser = await User.findById(id).exec();
-
-  if (!loginUser) {
-    throw createHttpError(404, "User not found");
-  }
-
-  const article = await Article.findOne({ slug }).exec();
-
-  if (!article) {
-    throw createHttpError(404, "Article not found");
-  }
-
-  await loginUser.unfavorite(article._id);
-  const updatedArticle = await article.updateFavoriteCount();
-
-  return res.status(200).json({
-    article: await updatedArticle.toArticleResponse(loginUser),
-  });
-};
-
-export const getArticleWithSlug: RequestHandler = async (req, res) => {
-  const { slug } = req.params;
-  const article = await Article.findOne({ slug }).exec();
-
-  if (!article) {
-    throw createHttpError(401, "Article not found");
-  }
-
-  res.status(200).json({
-    article: await article.toArticleResponse(false),
-  });
-};
-
-export const updateArticle: RequestHandler = async (req, res) => {
-  const newReq = req as unknown as JWTNewRequest;
-  const id = newReq.userId;
-  const { slug } = req.params;
-  const { article } = req.body;
-
-  const loginUser = await User.findById(id).exec();
-
-  if (!loginUser) {
-    throw createHttpError(404, "User not found");
-  }
-  const target = await Article.findOne({ slug }).exec();
-
-  if (loginUser._id.toString() !== target?.author.toString()) {
-    throw createHttpError(401, "You are not allowed to update this article");
-  }
-
-  if (target && article) {
-    if (article.title) target.title = article.title;
-    if (article.description) target.description = article.description;
-    if (article.body) target.body = article.body;
-
-    await target.save();
-    res.status(200).json({
-      article: await target.toArticleResponse(loginUser),
-    });
-  } else {
-    res.status(404).json({ message: "Article not found" });
+      await target.save();
+      res.status(200).json({
+        article: await target.toArticleResponse(loginUser),
+      });
+    } else {
+      res.status(404).json({ message: "Article not found" });
+    }
+  } catch (error) {
+    next(error);
   }
 };
